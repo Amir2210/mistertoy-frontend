d18f638fe1f2161f9b7b31f10b2e7cc3852ac2fb
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-12-25 22:46:29 +0200

    Update the detail page with more info about the toy

--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -8,7 +8,7 @@ export function ToyDetails(){
 
   useEffect(() => {
     loadToy()
-}, [toyId])
+}, [])
 
 function loadToy() {
   toyService.getById(toyId)
@@ -28,6 +28,10 @@ if (!toy) return <div>Loading...</div>
     <section className='toy-details'>
             <h1>Toy Name: {toy.name}</h1>
             <h1>Toy Price: {toy.price}</h1>
+            <h2>Toy Labels: </h2>
+            {toy.labels.map((label, index) =>{
+               return <h4 key={index}>{label}</h4>
+            })}
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quae fuga eveniet, quisquam ducimus modi optio in alias accusantium corrupti veritatis commodi tenetur voluptate deserunt nihil quibusdam. Expedita, architecto omnis?</p>
             <button onClick={onBack}>Back</button>
     </section>
