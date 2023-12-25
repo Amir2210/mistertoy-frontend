a0aff720d6e1170bc5b2e33978b95fa1e160ac74
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-12-25 16:42:06 +0200

    Create toy service

---
+++ src/services/toy.service.js
@@ -0,0 +1,73 @@
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+
+const TOY_KEY = 'toyDB'
+const labels = [
+  'On wheels',
+  'Box game',
+  'Art',
+  'Baby',
+  'Doll',
+  'Puzzle',
+  'Outdoor',
+  'Battery Powered'
+]
+
+export const toyService = {}
+
+_createToys()
+
+function query(filterBy = 'all') {
+  if (!filterBy) filterBy = 'all'
+  return storageService.query(TOY_KEY).then((toys) => {
+    return toys
+  })
+}
+
+function getById(toyId) {
+  return storageService.get(TOY_KEY, toyId)
+}
+
+function remove(toyId) {
+  return storageService.remove(TOY_KEY, toyId)
+}
+
+function save(toy) {
+  if (toy._id) {
+    return storageService.put(TOY_KEY, toy)
+  } else {
+    // toy.owner = userService.getLoggedinUser()
+    return storageService.post(TOY_KEY, toy)
+  }
+}
+
+function getEmptyToy(name = '', price = 1, labels = []) {
+  return {
+    name,
+    price,
+    labels,
+    createdAt: Date.now(),
+    inStock: true
+  }
+}
+
+function _createToys() {
+  let toys = storageService.loadFromStorage(TOY_KEY)
+  if (!toys || !toys.length) {
+    toys = []
+    toys.push(
+      _createToy('Talking Doll', 123, ['Doll', 'Battery Powered', 'Baby'])
+    )
+    toys.push(_createToy('Monopol', 555, ['Box game']))
+  }
+}
+
+function getDefaultFilter() {
+  return 'all'
+}
+
+function _createToy(name, price, label) {
+  const toy = getEmptyToy(name, price, label)
+  toy._id = utilService.makeId()
+  return toy
+}
